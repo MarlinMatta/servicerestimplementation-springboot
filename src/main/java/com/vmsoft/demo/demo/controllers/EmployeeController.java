560ecb21fa994d86494e1e88684c7c2e5e61f6d4
MarlinMatta <Monasterio.123>
2020-06-14 00:20:49 -0400

    creando endpoint

--- src/main/java/com/vmsoft/demo/demo/controllers/EmployeeController.java
+++ src/main/java/com/vmsoft/demo/demo/controllers/EmployeeController.java
@@ -2,9 +2,7 @@ package com.vmsoft.demo.demo.controllers;
 
 import com.vmsoft.demo.demo.utils.DataSource;
 import com.vmsoft.demo.demo.models.Employee;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +20,16 @@ public class EmployeeController {
     @RequestMapping(value = "/employees", method = RequestMethod.GET)
     public List<Employee> getEmployees() {
         return dataSource.getEmployees();
+    }
+
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public Employee getEmployee(@RequestParam int id) {
+        return dataSource.getEmployees().stream().filter(it -> it.getId() == id).findFirst().orElse(  null);
+    }
+
+    @RequestMapping(value = "/employee", method = RequestMethod.POST)
+    public String createEmployee(@RequestBody Employee employee) {
+       dataSource.getEmployees().add(employee);
+       return "OK";
     }
 }
